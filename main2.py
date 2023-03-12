63e0a6bae3bb0408d5b76a67837371c9d9143e58
rojasfuentes <108911511+rojasfuentes@users.noreply.github.com>
2023-03-12 16:34:12 -0600

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,6 +1,6 @@
+from threading import Thread
 import requests
 from bs4 import BeautifulSoup
-import threading
 
 DOMAIN = 'https://pokemondb.net'
 URL = '/pokedex/all'
@@ -16,38 +16,14 @@ def get_content(url):
     else:
         print('Error: ', response.status_code)
 
-
 def get_species_pokemon(url):
     soup = get_content(url)
 
     table = soup.find(
-        'table', {'class': 'vitals-table'})
+                'table', {'class': 'vitals-table'})
     species = table.tbody.find_all('tr')[2].td.text
 
     return species
-
-
-def process_pokemon(row):
-    cols = row.find_all('td')
-
-    name = cols[1].find('a').text
-    type = [a.text for a in cols[2].find_all('a')]
-    link = DOMAIN + cols[1].a['href']
-
-    species = get_species_pokemon(link)
-
-    print(name, '', type, '-', species)
-
-
-def process_rows(rows):
-    threads = []
-    for row in rows:
-        thread = threading.Thread(target=process_pokemon, args=(row,))
-        threads.append(thread)
-        thread.start()
-
-    for thread in threads:
-        thread.join()
 
 
 if __name__ == '__main__':
@@ -55,10 +31,18 @@ if __name__ == '__main__':
 
     table = soup.find('table', {'id': 'pokedex'})
 
-    # Dividing the table rows in smaller chunks for processing
-    rows_chunks = [table.find_all('tr')[i:i+5]
-                   for i in range(1, len(table.find_all('tr')), 5)]
+    threads = []
+    for row in table.find_all('tr')[1:15]:
 
-    # Process rows chunks in parallel
-    for chunk in rows_chunks:
-        process_rows(chunk)
+        cols = row.find_all('td')
+
+        name = cols[1].find('a').text
+        type = [a.text for a in cols[2].find_all('a')]
+        link = DOMAIN + cols[1].a['href']
+
+        t = Thread(target=get_species_pokemon, args=(link,))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
